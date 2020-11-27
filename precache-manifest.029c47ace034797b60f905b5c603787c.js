208dc1b44e6ee01ceb2f07daba020e5dc3f4d054
AbdelTrochez <abdel.atr@gmail.com>
2020-11-27 04:46:19 -0600

    Updates

---
+++ precache-manifest.029c47ace034797b60f905b5c603787c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0091d356af5de74a5e8b0a452f057e",
+    "url": "/actividad23plus_react_router/index.html"
+  },
+  {
+    "revision": "833d68174c85627fd18c",
+    "url": "/actividad23plus_react_router/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ca2e45c05f72a3fe161",
+    "url": "/actividad23plus_react_router/static/js/2.964cf816.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/actividad23plus_react_router/static/js/2.964cf816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833d68174c85627fd18c",
+    "url": "/actividad23plus_react_router/static/js/main.aba79ef1.chunk.js"
+  },
+  {
+    "revision": "a1fcfb621de3b6f09da1",
+    "url": "/actividad23plus_react_router/static/js/runtime-main.07d12485.js"
+  }
+]);
